ecfaa919cd0cd153c1ff77ffb91e4b00b1cf01bb
Sharon Lee <sharonshlee@icloud.com>
2021-09-29 16:07:22 -0400

    add the appointment component

---
+++ src/components/Appointment/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "Appointment/styles.scss";
+
+export default function Appointment(props) {
+  return <article className="appointment"></article>;
+}
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 
+import InterviewerList from "components/Appointment/index";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
@@ -132,3 +134,11 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+/// Appointment Component
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
