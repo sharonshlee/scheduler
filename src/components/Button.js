276e01f8978221740f9f0ffe8bc799b5be4238f7
Sharon Lee <sharonshlee@icloud.com>
2021-09-29 08:22:10 -0400

    add buttons default, confirm and danger

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+  //let buttonClass = "button";
+
+  // props.confirm && (buttonClass += " button--confirm");
+  // props.danger && (buttonClass += " button--danger");
+
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+
+  return (
+    <button {...props} className={buttonClass}>
+      {/* <button
+      {...props}
+      className={`button ${props.confirm ? "button--confirm" : ""}${props.danger ? "button--danger" : ""
+      }`}
+    > */}
+      {props.children}
+    </button>
+  );
 }
