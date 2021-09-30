9a14dad531db5e166a662a9129431fda5bac64ce
Sharon Lee <sharonshlee@icloud.com>
2021-09-30 16:29:03 -0400

    displaying appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview && <Show {...props.interview} />}
+      {!props.interview && <Empty />}
+    </article>
+  );
 }
