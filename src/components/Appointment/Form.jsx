e87b94e6d2fcf97d99dce05f1714c87042c3a146
Sharon Lee <sharonshlee@icloud.com>
2021-09-30 15:38:45 -0400

    adding state to the appointment form

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -4,15 +4,25 @@ import Button from "components/Button";
 import "components/Appointment/styles.scss";
 
 export default function Form(props) {
-  const { name, interviewer, interviewers, onSave, onCancel } = props;
+  const { interviewers, onSave, onCancel } = props;
 
-  const [name1, setName] = useState("");
-  const [interviewer1, setInterviewer] = useState(null);
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    onCancel();
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -30,12 +40,12 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={() => onCancel()} danger>
+          <Button onClick={() => cancel()} danger>
             Cancel
           </Button>
           <Button
             onClick={() => {
-              onSave({ name: name, interviewer: interviewer });
+              onSave(name, interviewer);
             }}
             confirm
           >
