27230558488cbb7878b27ee8147074591e8a672e
Sharon Lee <sharonshlee@icloud.com>
2021-10-01 19:21:26 -0400

    edit getAppointmentsForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,13 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.filter((item) => item.name === day);
 
   //... returns an array of appointments for that day
   const filteredAppointments = [];
   for (const filteredDay of filteredDays) {
     for (const appointment of filteredDay.appointments) {
-      filteredAppointments.push(state.appointments[appointment]);
+      if (state.appointments[appointment]) {
+        filteredAppointments.push(state.appointments[appointment]);
+      }
     }
   }
 
