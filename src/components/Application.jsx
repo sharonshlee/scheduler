ed400ca9c5bc4fa6f395c31806cc11f8676c3412
Sharon Lee <sharonshlee@icloud.com>
2021-10-01 09:20:36 -0400

    add api request days with axios

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -79,6 +62,14 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then((response) => {
+      console.log(response.data);
+      setDays(response.data);
+    });
+  }, []);
 
   const parsedAppointments = appointments.map((item) => {
     return (
