a8011b34c99b46ae9bd1271efc826fedd60fb932
Sharon Lee <sharonshlee@icloud.com>
2021-10-02 15:44:48 -0400

    use useVisualMode custom hook to cancel, show and create appointment form

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,15 +1,38 @@
 import React from "react";
+import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
-import "components/Appointment/styles.scss";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview && <Show {...props.interview} />}
-      {!props.interview && <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          name=""
+          interviewer=""
+          interviewers={[]} // getInterviewersForDay
+          onCancel={() => back()} //return to the EMPTY state
+        />
+      )}
     </article>
   );
 }
