e20528bfad13421f974b837010b5190616164e72
Sharon Lee <sharonshlee@icloud.com>
2021-10-02 15:42:47 -0400

    codes clean up

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,6 @@ import "components/Button.scss";
 import classNames from "classnames";
 
 export default function Button(props) {
-  //let buttonClass = "button";
-
-  // props.confirm && (buttonClass += " button--confirm");
-  // props.danger && (buttonClass += " button--danger");
-
   const buttonClass = classNames("button", {
     "button--confirm": props.confirm,
     "button--danger": props.danger,
@@ -15,11 +10,6 @@ export default function Button(props) {
 
   return (
     <button {...props} className={buttonClass}>
-      {/* <button
-      {...props}
-      className={`button ${props.confirm ? "button--confirm" : ""}${props.danger ? "button--danger" : ""
-      }`}
-    > */}
       {props.children}
     </button>
   );
